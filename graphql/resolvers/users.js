1051a1bdfe1ee48cf6afc89a5d09dcae020f19ad
alecbrando <37994059+alecbrando@users.noreply.github.com>
2021-04-18 19:50:53 -0700

    added validator for user entry

--- graphql/resolvers/users.js
+++ graphql/resolvers/users.js
@@ -1,7 +1,8 @@
 import User from "../../models/User.js";
 import bcrypt from "bcryptjs";
 import jwt from "jsonwebtoken";
-
+import pkg from "apollo-server";
+const { UserInputError } = pkg;
 import config from "../../config.js";
 const { SECRET_KEY } = config;
 
@@ -9,12 +10,18 @@ export default {
   Mutation: {
     async register(
       parent,
-      { registerInput: { username, email, password, confirmedPassword } },
-      context,
-      info
+      { registerInput: { username, email, password, confirmedPassword } }
     ) {
       //TODO: Validate user data
       //TODO: Make sure uer doesn't already exist
+      const user = await User.findOne({ username });
+      if (user) {
+        throw new UserInputError("Username is already taken", {
+          errors: {
+            username: "This username is taken",
+          },
+        });
+      }
       //TODO: hash password and create an auth token
       password = await bcrypt.hash(password, 12);
       const newUser = new User({
---
+++ util/validators.js
@@ -0,0 +1,26 @@
+const validateRegisterInput = (username, email, password, confirmPassword) => {
+  const errors = {};
+  if (username.trim() === "") {
+    errors.username = "Username must not be empty!";
+  }
+  if (email.trim() === "") {
+    errors.email = "Email must not be empty!";
+  } else {
+    const regEx = /^([0-9a-zA-Z]([-.\w]*[0-9a-zA-Z])*@([0-9a-zA-Z][-\w]*[0-9a-zA-Z]\.)+[a-zA-Z]{2,9})$/;
+    if (!email.match(regEx)) {
+      errors.email = "Email must be a valid email address!";
+    }
+  }
+  if (password.trim() === "") {
+    errors.password = "Password can't be empty!";
+  } else if (password !== confirmPassword) {
+    errors.password = "Passwords don't match!";
+  }
+
+  return {
+    errors,
+    valid: Object.keys(errors).length === 0,
+  };
+};
+
+export default validateRegisterInput;
